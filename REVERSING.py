85a25ad563c5a0265b6db7538013920d8ae7227a
Abhishek Kaushal <abhishek.k98820@gmail.com>
2024-02-17 19:57:21 +0530

    2n commit

--- REVERSING.py
+++ REVERSING.py
@@ -3,6 +3,7 @@ x=int(input("enetr the elements you want to add: "))
 for i in range(0,x):
     y=input("eneter the elements: ")
     n.append(y)
+    
 print(n)
 n.reverse()
 print(n)
--- pattern2.py
+++ pattern2.py
@@ -2,10 +2,10 @@ n=5
 for i in range(0,n+1):
     for k in range(n-i,0,-1):
         print(" ",end=(" "))
-    for s in range(0,i):
+    for s in range(0,i*2-1):
         print("*",end=(" "))
     
-    for s in range(1,i):
-        print("*",end=(" "))
+    # for s in range(1,i):
+    #     print("*",end=(" "))
     print()
     
