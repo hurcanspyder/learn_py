09545f2b591789cfcb55a9025206e17312c8a35b
Abhishek Kaushal <abhishek.k98820@gmail.com>
2024-02-18 12:57:38 +0530

    learn import statement, simple intrest program

--- pattern2.py
+++ pattern2.py
@@ -1,11 +1,19 @@
-n=5
-for i in range(0,n+1):
-    for k in range(n-i,0,-1):
-        print(" ",end=(" "))
-    for s in range(0,i*2-1):
-        print("*",end=(" "))
+def new_func():
+    n=5
+    for i in range(1,n+1):
+        for k in range(n-i,0,-1):
+            print(" ",end=(" "))
+        for s in range(0,i*2-1):
+            print("*",end=(" "))
+        print()
+    for g in range(1,n+1):
+        for j in range(0,g):
+            print(" ",end=(" "))
+        for u in range(n-g,0,-1):
+            print("*",end=(" "))
+        for u in range(n-g-1,0,-1):
+            print("*",end=(" "))
     
-    # for s in range(1,i):
-    #     print("*",end=(" "))
-    print()
-    
+        print()
+
+new_func()
--- simpleint.py
+++ simpleint.py
@@ -1,8 +1,13 @@
-p=int(input("enter the principle"))
-r=int(input("enter the rate"))
-t=int(input("enter the time"))
+r=int(input("enter the principle"))
+t=int(input("enter the rate"))
+p=int(input("enter the time"))
 def simple_interest(p,r,t):
     simple1_interest=p*r*t/100
     return simple1_interest
-a=simple_interest(p,r,t)
+a=simple_interest(r,t,p)
 print(a)
+# def simple2():
+#     g=p*r*t/100
+#     return g
+# c=simple2()
+# print(c)
